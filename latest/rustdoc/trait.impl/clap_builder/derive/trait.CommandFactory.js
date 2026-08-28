e87ff9092e7670a2eaff15f57405969d3bf160ec
docs-deploy <docs@tau.local>
2026-08-28 10:44:38 +0000

    chore(docs): clear latest before overlay

--- latest/rustdoc/trait.impl/clap_builder/derive/trait.CommandFactory.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    const implementors = Object.fromEntries([["tau_cli",[["impl CommandFactory for <a class=\"struct\" href=\"tau_cli/cli/struct.Cli.html\" title=\"struct tau_cli::cli::Cli\">Cli</a>",0]]],["xtask",[["impl CommandFactory for <a class=\"struct\" href=\"xtask/struct.Cli.html\" title=\"struct xtask::Cli\">Cli</a>",0]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":59,"fragment_lengths":[143,129]}
