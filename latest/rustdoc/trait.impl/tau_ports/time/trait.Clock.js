59d2f70d94dd67bde5af6bef53bd9d06bc3e4cb9
docs-deploy <docs@tau.local>
2026-08-29 09:50:46 +0000

    chore(docs): clear latest before overlay

--- latest/rustdoc/trait.impl/tau_ports/time/trait.Clock.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    const implementors = Object.fromEntries([["tau_embed_example",[["impl Clock for <a class=\"struct\" href=\"tau_embed_example/struct.SystemClock.html\" title=\"struct tau_embed_example::SystemClock\">SystemClock</a>",0]]],["tau_ports",[]],["tau_runtime_core",[]],["tau_runtime_tokio",[["impl Clock for <a class=\"struct\" href=\"tau_runtime_tokio/clock/struct.TokioClock.html\" title=\"struct tau_runtime_tokio::clock::TokioClock\">TokioClock</a>",0]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":59,"fragment_lengths":[179,17,24,190]}
--- latest/rustdoc/trait.impl/tau_wasm_host/trait.EmbedPorts.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    const implementors = Object.fromEntries([["tau_wasm_embed_example",[["impl <a class=\"trait\" href=\"tau_wasm_host/trait.EmbedPorts.html\" title=\"trait tau_wasm_host::EmbedPorts\">EmbedPorts</a> for <a class=\"struct\" href=\"tau_wasm_embed_example/struct.ProductPorts.html\" title=\"struct tau_wasm_embed_example::ProductPorts\">ProductPorts</a>",0]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":59,"fragment_lengths":[312]}
